d9e2abac344b0ca046fb67695bc61d569cce991e
Alexander Kutschera <alexander.kutschera@gmail.com>
2021-05-16 21:43:41 +0200

    added temp.py to atuomate script

--- rpi-script/temp.py
+++ rpi-script/temp.py
@@ -1,12 +1,9 @@
 # libraries
 import matplotlib.pyplot as plt
-#plt.style.use('seaborn-whitegrid')
 import numpy as np
 import pandas as pd
 import matplotlib.dates as mdates
 from matplotlib.dates import DateFormatter
-import seaborn as sns
-from plotnine import ggplot, geom_bar, aes, geom_point
 
 temp_data = pd.read_csv("temp.log", header=None)
 temp_data.columns = ("Date", "Temp")
@@ -16,36 +13,12 @@ temp_data["Date"] = pd.to_datetime(temp_data["Date"], format="%Y-%m-%d_%H:%M:%S"
 temp_data_morning = temp_data[temp_data['Date'].dt.hour < 11]
 temp_data_afternoon = temp_data[temp_data['Date'].dt.hour > 12]
 
-temp_data["Date"].max()
-temp_data[temp_data["Date"].dt.month == 10]
-
-monthly = pd.DataFrame(columns=['min_temp', 'max_temp', 'av_temp', 'month', 'year'])
-
-months = np.unique(temp_data["Date"].dt.month)
-years = np.unique(temp_data["Date"].dt.year)
-for year in years:
-    year_data = temp_data[temp_data["Date"].dt.year == year]
-    for month in months:
-        min = year_data[temp_data["Date"].dt.month == month].min()
-        max = year_data[temp_data["Date"].dt.month == month].max()
-        av = np.average(year_data[temp_data["Date"].dt.month == month]["Temp"])
-        print(max["Temp"], min["Temp"])
-        monthly = monthly.append({'min_temp': min["Temp"], 'max_temp': max["Temp"],
-            'av_temp': av, 'month': month, 'year': year}, ignore_index=True)
-
-
-print(monthly)
-
-#ggplot(monthly, aes('month','max_temp')) + geom_point()
-
 # Create figure and plot space
 fig, ax = plt.subplots(1,figsize=(18, 6))
 
 # Add x-axis and y-axis
-#ax.plot("Date","Temp", data=temp_data)
 ax.plot(temp_data_morning.Date, temp_data_morning.Temp, color="midnightblue", label="Morning")
 ax.plot(temp_data_afternoon.Date, temp_data_afternoon.Temp, color="darkviolet", label="Afternoon")
-# ax.plot(temp_data.Date, temp_data.Temp, color="black")
 
 # Set title and labels for axes
 ax.set(xlabel="",
@@ -74,44 +47,6 @@ ax.tick_params(axis='y', which='major', labelsize=15, direction='out', grid_alph
 ax.tick_params(axis='both', which='minor', labelsize=15,  direction='out', length=0, width=0, pad=15)
 ax.legend(frameon=False)
 
-#ax[1].bar(monthly[0])
 plt.savefig('temp_plot.png')
 
-
-# Create figure and plot space
-fig, ax = plt.subplots(1,figsize=(18, 6))
-
-# Add x-axis and y-axis
-#ax.plot("Date","Temp", data=temp_data)
-ax.plot(temp_data_morning.Date, temp_data_morning.Temp, color="midnightblue", label="Morning")
-ax.plot(temp_data_afternoon.Date, temp_data_afternoon.Temp, color="darkviolet", label="Afternoon")
-
-# Set title and labels for axes
-ax.set(xlabel="",
-       ylabel="Temperature [°C]",
-       title="Temperature over Time")
-
-ax.yaxis.label.set_size(15)
-ax.title.set_size(20)
-
-
-# Sho the last 30 days
-datemin = np.datetime64(temp_data['Date'].iloc[-1], 'D') -np.timedelta64(40, 'D')
-datemax = np.datetime64(temp_data['Date'].iloc[-1], 'D')
-ax.set_xlim(datemin, datemax)
-
-# Define the date format
-date_form_major = DateFormatter("%d. %B")
-date_form_minor = DateFormatter("%d")
-
-ax.xaxis.set_minor_formatter(date_form_minor)
-ax.xaxis.set_minor_locator(mdates.WeekdayLocator(interval=1))
-ax.xaxis.set_major_formatter(date_form_major)
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-
-ax.yaxis.grid(True)
-ax.tick_params(axis='x', which='major', labelsize=20, direction='out', length=8, width=1, pad=15)
-ax.tick_params(axis='y', which='major', labelsize=15, direction='out', grid_alpha=0.5, length=0, width=0, pad=15)
-ax.tick_params(axis='y', which='minor', labelsize=15,  direction='out', length=0, width=0, pad=15)
-ax.tick_params(axis='x', which='minor', labelsize=15,  direction='out', length=4, width=1, pad=15)
-ax.legend(frameon=False)
+print("successfully generated temperature plot")
